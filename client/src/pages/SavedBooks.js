e6f6131eb7c9bdbd1ad8d2a4602a27bfc01bb8b9
jessicashong <jessicashong12@gmail.com>
2023-03-21 20:13:11 -0500

    deploy to heroku

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Card,
@@ -7,7 +7,7 @@ import {
   Col
 } from 'react-bootstrap';
 
-// import { getMe, deleteBook } from '../utils/API';
+import { getMe, deleteBook } from '../utils/API';
 import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { QUERY_ME } from '../utils/queries';
@@ -15,13 +15,11 @@ import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
-const SavedBooks = () => {
-  // const [userData, setUserData] = useState({});
+const SavedBooks = ({ bookId }) => {
+  const [userData, setUserData] = useState({});
   const { _id } = useParams();
-  const { loading, data } = useQuery(QUERY_ME, {
-    variables: { me: _id },
-  });
-  const userData = data?.me || {};
+  const { loading, data } = useQuery(QUERY_ME);
+  // const userData = data?.me || {};
   const [removeBook, { error }] = useMutation(REMOVE_BOOK, {
     update(cache, { data: { removeBook } }){
       try {
@@ -39,30 +37,21 @@ const SavedBooks = () => {
   // use this to determine if `useEffect()` hook needs to run again
   // const userDataLength = Object.keys(userData).length;
 
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  //       if (!token) {
-  //         return false;
-  //       }
+        if (!token) {
+          return false;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-  //       const response = await me(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userData]);
+    getUserData();
+  }, [userData]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -96,7 +85,7 @@ const SavedBooks = () => {
       console.error(err);
     }
   };
-  
+  console.log(cache);
 
   // if data isn't here yet, say so
   if (loading) {
@@ -112,12 +101,12 @@ const SavedBooks = () => {
       </div>
       <Container>
         <h2 className='pt-5'>
-          {userData.savedBooks.length
+          {data.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {data.savedBooks.map((book) => {
             return (
               <Col md="4">
                 <Card key={book.bookId} border='dark'>
